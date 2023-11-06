6c8b79b7fb95a3e50cb6472d1f10db8a37341b78
Benly <benbenly26@gmail.com>
2023-11-06 16:18:07 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+export default function App() {
+
+  // inside the array object
+  const arr = [{
+    name: 'xxx',
+    age: '22',
+    year: '1999'
+  },
+  {
+    name: 'yyy',
+    age: '12',
+    year: '6999'
+  }]
+  // get the value from the object
+  console.log(arr);
+  // get any one value from any one object
+  console.log(arr[1].year);
+  // get the both object values using map function
+  console.log(
+    arr.map((val) => {
+      return val.name
+    })
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {/* get the obj values using map and key */}
+      {arr.map((val) => {
+        return <div key={val.name}>
+          <h4>{val.year}</h4>
+        </div>
+      })
+      };
+    </>
   );
 }
-
-export default App;
